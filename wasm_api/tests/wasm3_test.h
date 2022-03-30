788f462facf701930d670aa5bf5b5a4d82acd80a
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-03-29 23:35:50 -0700

    tests on memory set

--- wasm_api/tests/wasm3_test.h
+++ wasm_api/tests/wasm3_test.h
@@ -29,4 +29,41 @@ public:
 		TS_ASSERT_EQUALS(res, 35);
 	}
 
+	void test_set_memory()
+	{
+		wasm3::environment e;
+		auto c = load_wasm_from_file("wasm_api/tests/wat/test_set_memory.wasm");
+
+		auto m = e.parse_module(c.data(), c.size());
+
+		auto r = e.new_runtime(65536);
+
+		r.load(m);
+
+		auto sz = r.find_function("size");
+
+		TS_ASSERT_EQUALS(sz.template call<int32_t>(), 1);
+
+		auto store = r.find_function("store");
+
+		store.call(0, 0x12345678);
+
+		store.call(4, 0xABCDEF90);
+
+		auto load = r.find_function("load");
+
+		uint32_t mem0 = load.template call<int32_t>(0);
+
+		TS_ASSERT_EQUALS(mem0, 0x12345678);
+
+		auto load8 = r.find_function("load8");
+
+		TS_ASSERT_EQUALS(load8.template call<int32_t>(0), 0x78);
+		TS_ASSERT_EQUALS(load8.template call<int32_t>(1), 0x56);
+
+		auto load16 = r.find_function("load16");
+
+		TS_ASSERT_EQUALS(load16.template call<int32_t>(3), 0x9012);
+	}
+
 };
