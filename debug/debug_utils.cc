83134b5e263a2b99ee7393a2f92fdab57364866b
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-09-12 20:12:13 -0700

    debug macros

--- debug/debug_utils.cc
+++ debug/debug_utils.cc
@@ -2,30 +2,40 @@
 
 #include "mtt/trie/debug_macros.h"
 
-namespace debug
-{
+namespace debug {
 
-std::string 
+std::string
 storage_object_to_str(std::optional<scs::StorageObject> const& obj)
 {
-	if (!obj)
-	{
-		return "[nullopt]";
-	}
-	return storage_object_to_str(*obj);
+    if (!obj) {
+        return "[nullopt]";
+    }
+    return storage_object_to_str(*obj);
 }
 
-std::string 
+std::string
 storage_object_to_str(scs::StorageObject const& obj)
 {
-	switch(obj.type())
-	{
-		case scs::ObjectType::RAW_MEMORY:
-			return "[raw_memory: " + debug::array_to_str(obj.raw_memory_storage().data) + "]";
-		case scs::ObjectType::NONNEGATIVE_INT64:
-			return "[nn_int64: " + std::to_string(obj.nonnegative_int64()) + "]";	
-	}
-	throw std::runtime_error("unknown object type in storage_object_to_str");
+    switch (obj.type()) {
+        case scs::ObjectType::RAW_MEMORY:
+            return "[raw_memory: "
+                   + debug::array_to_str(obj.raw_memory_storage().data) + "]";
+        case scs::ObjectType::NONNEGATIVE_INT64:
+            return "[nn_int64: " + std::to_string(obj.nonnegative_int64())
+                   + "]";
+    }
+    throw std::runtime_error("unknown object type in storage_object_to_str");
 }
 
-} /* scs */
+std::string
+array_to_str(const unsigned char* array, const int len)
+{
+    std::stringstream s;
+    s.fill('0');
+    for (int i = 0; i < len; i++) {
+        s << std::setw(2) << std::hex << (unsigned short)array[i];
+    }
+    return s.str();
+}
+
+} // namespace debug
--- debug/debug_utils.h
+++ debug/debug_utils.h
@@ -1,17 +1,28 @@
 #pragma once
 
-#include <string>
 #include <optional>
+#include <string>
 
 #include "xdr/storage.h"
 
-namespace debug
-{
+namespace debug {
 
-std::string 
+std::string
 storage_object_to_str(std::optional<scs::StorageObject> const& obj);
 
-std::string 
+std::string
 storage_object_to_str(scs::StorageObject const& obj);
 
-} /* debug */
+//! Convert a byte array to a hex string.
+std::string
+array_to_str(const unsigned char* array, const int len);
+
+template<typename ArrayLike>
+[[maybe_unused]] 
+static std::string
+array_to_str(const ArrayLike& array)
+{
+    return array_to_str(array.data(), array.size());
+}
+
+} // namespace debug
