d576055ea2d5ef432ddfc2abda9e66dcaea7ff38
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-10-25 16:43:47 -0700

    format

--- crypto/crypto_utils.cc
+++ crypto/crypto_utils.cc
@@ -1,44 +1,37 @@
 #include "crypto/crypto_utils.h"
 
-#include <sodium.h>
 #include <cstdio>
+#include <sodium.h>
 #include <stdexcept>
 
-namespace scs
-{
+namespace scs {
 
 uint8_t* HASH_KEY;
 
-//static uint8_t HASH_KEY[crypto_shorthash_KEYBYTES];
-
-void 
+void
 initialize_crypto()
 {
-	int res = sodium_init();
-	if (res == -1)
-	{
-		throw std::runtime_error("failed to init sodium");
-	}
-	if (res == 0)
-	{
-		HASH_KEY = new uint8_t[crypto_shorthash_KEYBYTES];
-		crypto_shorthash_keygen(HASH_KEY);
-	}
+    int res = sodium_init();
+    if (res == -1) {
+        throw std::runtime_error("failed to init sodium");
+    }
+    if (res == 0) {
+        HASH_KEY = new uint8_t[crypto_shorthash_KEYBYTES];
+        crypto_shorthash_keygen(HASH_KEY);
+    }
 
-	std::printf("initialized sodium\n");
+    std::printf("initialized sodium\n");
 }
 
 uint32_t
 shorthash(const uint8_t* data, uint32_t data_len, const uint32_t modulus)
 {
-	static_assert(crypto_shorthash_BYTES == 8);
+    static_assert(crypto_shorthash_BYTES == 8);
 
     uint64_t hash_out;
 
-    if (crypto_shorthash(reinterpret_cast<uint8_t*>(&hash_out),
-                         data,
-                         data_len,
-                         HASH_KEY)
+    if (crypto_shorthash(
+            reinterpret_cast<uint8_t*>(&hash_out), data, data_len, HASH_KEY)
         != 0) {
         throw std::runtime_error("shorthash fail");
     }
@@ -49,4 +42,4 @@ shorthash(const uint8_t* data, uint32_t data_len, const uint32_t modulus)
            >> 32;
 }
 
-} /* scs */
+} // namespace scs
--- crypto/crypto_utils.h
+++ crypto/crypto_utils.h
@@ -2,15 +2,12 @@
 
 #include <cstdint>
 
-namespace scs
-{
+namespace scs {
 
-void 
-__attribute__((constructor)) 
-initialize_crypto();
+void __attribute__((constructor)) initialize_crypto();
 
 // uses shared key randomly generated at startup
 uint32_t
 shorthash(const uint8_t* data, uint32_t data_len, const uint32_t modulus);
 
-} /* scs */
+} // namespace scs
