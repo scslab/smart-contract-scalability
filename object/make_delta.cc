42d92dd2f5e8b4bdd2eed43c47085d56eba1661f
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-10-26 01:26:26 -0700

    make hashset deltas

--- object/make_delta.cc
+++ object/make_delta.cc
@@ -1,30 +1,58 @@
 #include "object/make_delta.h"
 
-namespace scs
-{
+namespace scs {
 
-StorageDelta make_raw_memory_write(xdr::opaque_vec<RAW_MEMORY_MAX_LEN>&& data)
+StorageDelta
+make_raw_memory_write(xdr::opaque_vec<RAW_MEMORY_MAX_LEN>&& data)
 {
-	StorageDelta d;
-	d.type(DeltaType::RAW_MEMORY_WRITE);
-	d.data() = std::move(data);
-	return d;
+    StorageDelta d;
+    d.type(DeltaType::RAW_MEMORY_WRITE);
+    d.data() = std::move(data);
+    return d;
 }
 
-StorageDelta make_delete_last()
+StorageDelta
+make_delete_last()
 {
-	StorageDelta d;
-	d.type(DeltaType::DELETE_LAST);
-	return d;
+    StorageDelta d;
+    d.type(DeltaType::DELETE_LAST);
+    return d;
 }
 
-StorageDelta make_nonnegative_int64_set_add(int64_t set, int64_t add)
+StorageDelta
+make_nonnegative_int64_set_add(int64_t set, int64_t add)
 {
-	StorageDelta d;
-	d.type(DeltaType::NONNEGATIVE_INT64_SET_ADD);
-	d.set_add_nonnegative_int64().set_value = set;
-	d.set_add_nonnegative_int64().delta = add;
-	return d;
+    StorageDelta d;
+    d.type(DeltaType::NONNEGATIVE_INT64_SET_ADD);
+    d.set_add_nonnegative_int64().set_value = set;
+    d.set_add_nonnegative_int64().delta = add;
+    return d;
 }
 
-} /* scs */
+StorageDelta
+make_hash_set_insert(Hash const& h)
+{
+    StorageDelta d;
+    d.type(DeltaType::HASH_SET_INSERT);
+    d.hash() = h;
+    return d;
+}
+
+StorageDelta
+make_hash_set_increase_limit(uint16_t limit)
+{
+    StorageDelta d;
+    d.type(DeltaType::HASH_SET_INCREASE_LIMIT);
+    d.limit_increase() = limit;
+    return d;
+}
+
+StorageDelta
+make_hash_set_clear()
+{
+    StorageDelta d;
+    d.type(DeltaType::HASH_SET_CLEAR);
+    return d;
+}
+
+} // namespace scs
--- object/make_delta.h
+++ object/make_delta.h
@@ -6,13 +6,24 @@
 
 #include "xdr/storage_delta.h"
 
-namespace scs
-{
+namespace scs {
 
-StorageDelta make_raw_memory_write(xdr::opaque_vec<RAW_MEMORY_MAX_LEN>&& data);
+StorageDelta
+make_raw_memory_write(xdr::opaque_vec<RAW_MEMORY_MAX_LEN>&& data);
 
-StorageDelta make_delete_last();
+StorageDelta
+make_delete_last();
 
-StorageDelta make_nonnegative_int64_set_add(int64_t set, int64_t add);
+StorageDelta
+make_nonnegative_int64_set_add(int64_t set, int64_t add);
 
-} /* scs */
+StorageDelta
+make_hash_set_insert(Hash const& h);
+
+StorageDelta
+make_hash_set_increase_limit(uint16_t limit);
+
+StorageDelta
+make_hash_set_clear();
+
+} // namespace scs
