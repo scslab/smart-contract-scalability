8ab745c893418e9e12fe0d5fb4c187ec1e414400
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-10-27 14:11:09 -0700

    refactor proxyapplicator to support multiple deltatype per object type

--- storage_proxy/proxy_applicator.cc
+++ storage_proxy/proxy_applicator.cc
@@ -2,377 +2,430 @@
 
 #include "object/object_defaults.h"
 
-#include "debug/debug_utils.h"
 #include "debug/debug_macros.h"
+#include "debug/debug_utils.h"
 
 #include "xdr/storage_delta.h"
 
 #include "object/make_delta.h"
 
-namespace scs
-{
+namespace scs {
 
 bool
 ProxyApplicator::delta_apply_type_guard(StorageDelta const& d) const
 {
-	if (!current)
-	{
-		return true;
-	}
-	switch(d.type())
-	{
-		case DeltaType::RAW_MEMORY_WRITE:
-		{
-			return current -> body.type() == ObjectType::RAW_MEMORY;
-		}
-		case DeltaType::NONNEGATIVE_INT64_SET_ADD:
-		{
-			return current -> body.type() == ObjectType::NONNEGATIVE_INT64;
-		}
-		default:
-			throw std::runtime_error("unknown deltatype");
-	}
+    if (!current) {
+        return true;
+    }
+    switch (d.type()) {
+        case DeltaType::RAW_MEMORY_WRITE: {
+            return current->body.type() == ObjectType::RAW_MEMORY;
+        }
+        case DeltaType::NONNEGATIVE_INT64_SET_ADD: {
+            return current->body.type() == ObjectType::NONNEGATIVE_INT64;
+        }
+        default:
+            throw std::runtime_error("unknown deltatype");
+    }
 }
 
-std::optional<StorageDelta>
+std::optional<set_add_t>
 make_nnint64_delta(int64_t base, int64_t old_delta, int64_t new_delta)
 {
-	if (new_delta < 0)
-	{
-		if (__builtin_add_overflow_p(new_delta, old_delta, static_cast<int64_t>(0)))
-		{
-			std::printf("overflow on delta\n");
-			return std::nullopt;
-		}
+    if (new_delta < 0) {
+        if (__builtin_add_overflow_p(
+                new_delta, old_delta, static_cast<int64_t>(0))) {
+            std::printf("overflow on delta\n");
+            return std::nullopt;
+        }
 
-		new_delta += old_delta;
+        new_delta += old_delta;
 
-		if (__builtin_add_overflow_p(base, new_delta, static_cast<int64_t>(0)))
-		{			
-			std::printf("overflow on base + delta\n");
+        if (__builtin_add_overflow_p(
+                base, new_delta, static_cast<int64_t>(0))) {
+            std::printf("overflow on base + delta\n");
 
-			if (new_delta < 0)
-			{
-				// base + new_delta < INT64_MIN
-				return std::nullopt;
-			} else
-			{
-				// base + new_delta > INT64_MAX
+            if (new_delta < 0) {
+                // base + new_delta < INT64_MIN
+                return std::nullopt;
+            } else {
+                // base + new_delta > INT64_MAX
 
-				// reject if and only if base+old_delta < 0
-				// but base + old_delta < 0 only if base < 0
+                // reject if and only if base+old_delta < 0
+                // but base + old_delta < 0 only if base < 0
 
-				if (base < 0)
-				{
-					if (old_delta < 0)
-					{
-						throw std::runtime_error("invalid input to make_nnint64_delta");
-					}
+                if (base < 0) {
+                    if (old_delta < 0) {
+                        throw std::runtime_error(
+                            "invalid input to make_nnint64_delta");
+                    }
 
-					return std::nullopt;
-				}
+                    return std::nullopt;
+                }
 
-				return make_nonnegative_int64_set_add(base, new_delta);
-			}
-		}
-		if (base + new_delta < 0)
-		{
-			std::printf("negative result\n");
-			return std::nullopt;
-		}
+                set_add_t out;
+                out.set_value = base;
+                out.delta = new_delta;
+                return out;
 
-		return make_nonnegative_int64_set_add(base, new_delta);
-	}
-	//else new_delta >= 0
-	if (__builtin_add_overflow_p(old_delta, new_delta, static_cast<int64_t>(0)))
-	{
-		new_delta = INT64_MAX;
-	} else
-	{
-		new_delta += old_delta;
-	}
+                // return make_nonnegative_int64_set_add(base, new_delta);
+            }
+        }
+        if (base + new_delta < 0) {
+            std::printf("negative result\n");
+            return std::nullopt;
+        }
 
-	return make_nonnegative_int64_set_add(base, new_delta);
+        set_add_t out;
+        out.set_value = base;
+        out.delta = new_delta;
+        return out;
+
+        // return make_nonnegative_int64_set_add(base, new_delta);
+    }
+    // else new_delta >= 0
+    if (__builtin_add_overflow_p(
+            old_delta, new_delta, static_cast<int64_t>(0))) {
+        new_delta = INT64_MAX;
+    } else {
+        new_delta += old_delta;
+    }
+
+    set_add_t out;
+    out.set_value = base;
+    out.delta = new_delta;
+    return out;
+
+    // return make_nonnegative_int64_set_add(base, new_delta);
+}
+
+void
+ProxyApplicator::make_current(ObjectType obj_type)
+{
+    if (!current) {
+        current = StorageObject();
+        current->body.type(obj_type);
+    }
+    if (current->body.type() != obj_type) {
+        throw std::runtime_error("type mismatch");
+    }
+}
+
+void
+ProxyApplicator::make_current_nnint64(set_add_t const& delta)
+{
+    make_current(ObjectType::NONNEGATIVE_INT64);
+
+    if (__builtin_add_overflow_p(
+            delta.set_value, delta.delta, static_cast<int64_t>(0))) {
+        if (delta.delta > 0) {
+            current->body.nonnegative_int64() = INT64_MAX;
+        } else {
+            throw std::runtime_error("should not have negative overflow!");
+        }
+    } else {
+        current->body.nonnegative_int64() = delta.set_value + delta.delta;
+    }
 }
 
 bool
 ProxyApplicator::try_apply(StorageDelta const& d)
 {
-	if (d.type() == DeltaType::DELETE_LAST)
-	{
-		is_deleted = true;
-		current = std::nullopt;
-		return true;
-	}
+    if (d.type() == DeltaType::DELETE_LAST) {
+        is_deleted = true;
+        current = std::nullopt;
+        return true;
+    }
 
-	if (is_deleted)
-	{
-		return false;
-	}
+    if (is_deleted) {
+        return false;
+    }
 
-	if (!delta_apply_type_guard(d))
-	{
-		return false;
-	}
+    if (!delta_apply_type_guard(d)) {
+        return false;
+    }
 
-	switch(d.type())
-	{
-		case DeltaType::RAW_MEMORY_WRITE:
-		{
-			overall_delta = d;
-			break;
-		}
-		case DeltaType::NONNEGATIVE_INT64_SET_ADD:
-		{
-			if (!overall_delta)
-			{
-				auto res = make_nnint64_delta(d.set_add_nonnegative_int64().set_value, 0, d.set_add_nonnegative_int64().delta);
-				if (res)
-				{
-					overall_delta = res;
-					break;
-				} else
-				{
-					std::printf("no overall_delta return false;\n");
-					return false;
-				}
-			} 
-			else
-			{
-				if (overall_delta->set_add_nonnegative_int64().set_value != d.set_add_nonnegative_int64().set_value)
-				{
-					std::printf("base mismatch return false;\n");
+    switch (d.type()) {
+        case DeltaType::RAW_MEMORY_WRITE: {
+            if (!memory_write) {
+                memory_write = RawMemoryStorage();
+            }
+            memory_write->data = d.data();
+            make_current(ObjectType::RAW_MEMORY);
+            current->body.raw_memory_storage() = *memory_write;
+            return true;
+        }
+        case DeltaType::NONNEGATIVE_INT64_SET_ADD: {
+            if (!nnint64_delta) {
+                auto res = make_nnint64_delta(
+                    d.set_add_nonnegative_int64().set_value,
+                    0,
+                    d.set_add_nonnegative_int64().delta);
+                if (res) {
+                    nnint64_delta = *res;
+                    make_current_nnint64(*nnint64_delta);
+                    return true;
+                } else {
+                    std::printf("no overall_delta return false;\n");
+                    return false;
+                }
+            } else {
+                if (nnint64_delta->set_value
+                    != d.set_add_nonnegative_int64().set_value) {
+                    std::printf("base mismatch return false;\n");
 
-					return false;
-				}
+                    return false;
+                }
 
-				auto res = make_nnint64_delta(
-					overall_delta -> set_add_nonnegative_int64().set_value,
-					overall_delta -> set_add_nonnegative_int64().delta,
-					d.set_add_nonnegative_int64().delta);
+                auto res
+                    = make_nnint64_delta(nnint64_delta->set_value,
+                                         nnint64_delta->delta,
+                                         d.set_add_nonnegative_int64().delta);
 
-				if (!res)
-				{
-					std::printf("has overall_delta return false;\n");
-					return false;
-				}
-				overall_delta = *res;
-				break;
-			}
-		}
-		default:
-			throw std::runtime_error("unknown deltatype");
-	}
+                if (!res) {
+                    std::printf("has overall_delta return false;\n");
+                    return false;
+                }
+                nnint64_delta = *res;
+                make_current_nnint64(*nnint64_delta);
+                return true;
+            }
+        }
+        default:
+            throw std::runtime_error("unknown deltatype");
+    }
 
-	if (!overall_delta)
-	{
-		throw std::runtime_error("should not reach end of try_apply without an overall_delta");
-	}
+    throw std::runtime_error("unreachable");
+    /*
+            if (!overall_delta)
+            {
+                    throw std::runtime_error("should not reach end of try_apply
+       without an overall_delta");
+            }
 
-	current = make_object_from_delta(*overall_delta);
-	return true;
+            current = make_object_from_delta(*overall_delta);
+            return true; */
 }
 
 /*
-bool 
+bool
 ProxyApplicator::try_apply(StorageDelta const& d)
 {
-	struct resetter
-	{
-		bool do_reset_base = false;
+        struct resetter
+        {
+                bool do_reset_base = false;
 
-		std::optional<StorageObject>& base;
+                std::optional<StorageObject>& base;
 
-		resetter(std::optional<StorageObject>& base)
-			: base(base)
-			{}
-		~resetter()
-		{
-			if (do_reset_base) {
-				base = std::nullopt;
-			}
-		}
+                resetter(std::optional<StorageObject>& base)
+                        : base(base)
+                        {}
+                ~resetter()
+                {
+                        if (do_reset_base) {
+                                base = std::nullopt;
+                        }
+                }
 
-		void clear()
-		{
-			do_reset_base = false;
-		}
-	};
+                void clear()
+                {
+                        do_reset_base = false;
+                }
+        };
 
-	resetter r(base);
+        resetter r(base);
 
-	if (!typeclass.can_accept(d))
-	{
-		return false;
-	}
+        if (!typeclass.can_accept(d))
+        {
+                return false;
+        }
 
-	if (!base)
-	{
-		r.do_reset_base = true;
-		base = make_default_object_by_delta(d);
-	}
+        if (!base)
+        {
+                r.do_reset_base = true;
+                base = make_default_object_by_delta(d);
+        }
 
-	if (d.type() == DeltaType::DELETE_LAST)
-	{
-		base = std::nullopt;
-		is_deleted = true;
-	}
-	else
-	{
-		if (is_deleted)
-		{
-			return false;
-		}
+        if (d.type() == DeltaType::DELETE_LAST)
+        {
+                base = std::nullopt;
+                is_deleted = true;
+        }
+        else
+        {
+                if (is_deleted)
+                {
+                        return false;
+                }
 
-		if (!base.has_value())
-		{
-			throw std::runtime_error("no value when there should be");
-		}
+                if (!base.has_value())
+                {
+                        throw std::runtime_error("no value when there should
+be");
+                }
 
-		switch(base -> type())
-		{
-			case ObjectType::RAW_MEMORY:
-			{
-				if (!mem_set_called)
-				{
-					base -> raw_memory_storage().data = d.data();
-					mem_set_called = true;
-				}
-				else if (d.data() != base -> raw_memory_storage().data)
-				{
-					return false;
-				}
-			}
-			break;
-			case ObjectType::NONNEGATIVE_INT64:
-			{
-				static_assert(sizeof(long long) == 8, "int width mismatch");
+                switch(base -> type())
+                {
+                        case ObjectType::RAW_MEMORY:
+                        {
+                                if (!mem_set_called)
+                                {
+                                        base -> raw_memory_storage().data =
+d.data(); mem_set_called = true;
+                                }
+                                else if (d.data() != base ->
+raw_memory_storage().data)
+                                {
+                                        return false;
+                                }
+                        }
+                        break;
+                        case ObjectType::NONNEGATIVE_INT64:
+                        {
+                                static_assert(sizeof(long long) == 8, "int width
+mismatch");
 
-				int64_t base_val = d.set_add_nonnegative_int64().set_value;
-				int64_t delta = d.set_add_nonnegative_int64().delta;
+                                int64_t base_val =
+d.set_add_nonnegative_int64().set_value; int64_t delta =
+d.set_add_nonnegative_int64().delta;
 
-				// typeclass check means that all deltas that get this far have the same
-				// set_value
+                                // typeclass check means that all deltas that
+get this far have the same
+                                // set_value
 
-				int64_t prev_value = base -> nonnegative_int64();
-				if (!nn_int64_set_called)
-				{
-					prev_value = base_val;
-				}
+                                int64_t prev_value = base ->
+nonnegative_int64(); if (!nn_int64_set_called)
+                                {
+                                        prev_value = base_val;
+                                }
 
-				if (delta < 0)
-				{
-					if (delta == INT64_MIN || base_val < -delta)
-					{
-						std::printf("returning bc invalid delta\n");
-						// such a delta is always invalid, should always be rejected
-						// can't take -INT64_MIN
-						return false;
-					}
+                                if (delta < 0)
+                                {
+                                        if (delta == INT64_MIN || base_val <
+-delta)
+                                        {
+                                                std::printf("returning bc
+invalid delta\n");
+                                                // such a delta is always
+invalid, should always be rejected
+                                                // can't take -INT64_MIN
+                                                return false;
+                                        }
 
-					if (__builtin_add_overflow_p(prev_value, delta, static_cast<int64_t>(0)))
-					{
-						return false;
-					}
+                                        if (__builtin_add_overflow_p(prev_value,
+delta, static_cast<int64_t>(0)))
+                                        {
+                                                return false;
+                                        }
 
-					prev_value += delta;
-					if (prev_value < 0)
-					{
-						return false;
-					}
-				}
-				else
-				{
-					if (__builtin_add_overflow_p(prev_value, delta, static_cast<int64_t>(0)))
-					{
-						prev_value = INT64_MAX;
-					} 
-					else
-					{
-						prev_value += delta;
-					}
-				}
+                                        prev_value += delta;
+                                        if (prev_value < 0)
+                                        {
+                                                return false;
+                                        }
+                                }
+                                else
+                                {
+                                        if (__builtin_add_overflow_p(prev_value,
+delta, static_cast<int64_t>(0)))
+                                        {
+                                                prev_value = INT64_MAX;
+                                        }
+                                        else
+                                        {
+                                                prev_value += delta;
+                                        }
+                                }
 
-				base -> nonnegative_int64() = prev_value;
-				nn_int64_set_called = true;
-			}
-			break;
+                                base -> nonnegative_int64() = prev_value;
+                                nn_int64_set_called = true;
+                        }
+                        break;
 
-			default:
-				throw std::runtime_error("unknown object type");
-		}
-	}
-	typeclass.add(d);
-	r.clear();
-	return true;
+                        default:
+                                throw std::runtime_error("unknown object type");
+                }
+        }
+        typeclass.add(d);
+        r.clear();
+        return true;
 }
 */
 
 std::optional<StorageObject> const&
 ProxyApplicator::get() const
 {
-	// deletions are always applied at end
-	// can't materialize the deletion
-	// internally until we know no more deltas
-	// will be applied (i.e. until the obj is
-	// taken by the application for use elsewhere).
-	if (is_deleted)
-	{
-		OBJECT_INFO("returning deleted object");
-		return null_obj;
-	}
+    // deletions are always applied at end
+    // can't materialize the deletion
+    // internally until we know no more deltas
+    // will be applied (i.e. until the obj is
+    // taken by the application for use elsewhere).
+    if (is_deleted) {
+        OBJECT_INFO("returning deleted object");
+        return null_obj;
+    }
 
-	OBJECT_INFO("returning %s", debug::storage_object_to_str(current).c_str());
-	return current;
+    OBJECT_INFO("returning %s", debug::storage_object_to_str(current).c_str());
+    return current;
 }
 
 std::vector<StorageDelta>
 ProxyApplicator::get_deltas() const
 {
-	if (!overall_delta)
-	{
-		if (is_deleted)
-		{
-			//std::printf("ProxyApplicator::get_deltas: return delete_last (no delta)\n");
-			return { make_delete_last() };
-		}
-		//std::printf("ProxyApplicator::get_deltas: return null (no delta)\n");
-		return {};
-	}
+    if (!is_deleted && (!current)) {
+        return {};
+    }
 
-	if (is_deleted)
-	{
-		//std::printf("ProxyApplicator::get_deltas: return (delta, delete_last)\n");
-		return { *overall_delta, make_delete_last() };
-	}
+    std::vector<StorageDelta> out;
 
-	//std::printf("ProxyApplicator::get_deltas: return delete_last\n");
+    if (memory_write) {
+        out.push_back(make_raw_memory_write(
+            xdr::opaque_vec<RAW_MEMORY_MAX_LEN>(memory_write->data)));
+    }
 
-	return { *overall_delta };
+    if (nnint64_delta) {
+        out.push_back(make_nonnegative_int64_set_add(nnint64_delta->set_value,
+                                                     nnint64_delta->delta));
+    }
+
+    if (hs_size_increase > 0) {
+        static_assert(
+            MAX_HASH_SET_SIZE <= UINT16_MAX,
+            "change uint16 type on make_hash_set_increase_limit to wider uint");
+        out.push_back(make_hash_set_increase_limit(std::min(
+            hs_size_increase, static_cast<uint64_t>(MAX_HASH_SET_SIZE))));
+    }
+
+    for (auto const& h : new_hashes) {
+        out.push_back(make_hash_set_insert(h));
+    }
+
+    if (do_hs_clear) {
+        out.push_back(make_hash_set_clear());
+    }
+
+    if (is_deleted) {
+        out.push_back(make_delete_last());
+    }
+
+    return out;
 }
 
-std::optional<int64_t> 
+std::optional<int64_t>
 ProxyApplicator::get_base_nnint64_set_value() const
 {
-	if (overall_delta)
-	{
-		if (overall_delta -> type() != DeltaType::NONNEGATIVE_INT64_SET_ADD)
-		{
-			return std::nullopt;
-		}
+    if (nnint64_delta)
+    {
+        return nnint64_delta->set_value;
+    }
 
-		return overall_delta -> set_add_nonnegative_int64().set_value;
-	}
-
-	if (current)
-	{
-		if (current -> body.type() != ObjectType::NONNEGATIVE_INT64)
-		{
-			return std::nullopt;
-		}
-		return current -> body.nonnegative_int64();
-	}
-	return 0;
+    if (current) {
+        if (current->body.type() != ObjectType::NONNEGATIVE_INT64) {
+            return std::nullopt;
+        }
+        return current->body.nonnegative_int64();
+    }
+    return 0;
 }
 
-} /* scs */
+} // namespace scs
--- storage_proxy/proxy_applicator.h
+++ storage_proxy/proxy_applicator.h
@@ -5,47 +5,63 @@
 #include "xdr/storage.h"
 #include "xdr/storage_delta.h"
 
-namespace scs
-{
+namespace scs {
 
 class StorageDelta;
 
 class ProxyApplicator
 {
-	#if __cpp_lib_optional >= 202106L
-		constexpr static std::optional<StorageObject> null_obj = std::nullopt;
-	#else
-		const std::optional<StorageObject> null_obj = std::nullopt;
-	#endif
+#if __cpp_lib_optional >= 202106L
+    constexpr static std::optional<StorageObject> null_obj = std::nullopt;
+#else
+    const std::optional<StorageObject> null_obj = std::nullopt;
+#endif
 
-	std::optional<StorageObject> current;
+    std::optional<StorageObject> current;
 
-	std::optional<StorageDelta> overall_delta;
-	bool is_deleted = false;
+    // memory write
 
-	bool delta_apply_type_guard(StorageDelta const& d) const;
+    std::optional<RawMemoryStorage> memory_write;
 
-public:
+    // nnint64
 
-	ProxyApplicator(std::optional<StorageObject> const& base)
-		: current(base)
-		, overall_delta(std::nullopt)
-		{}
+    std::optional<set_add_t> nnint64_delta;
 
-	// no-op if result is false, apply if result is true
-	bool 
-	__attribute__((warn_unused_result))
-	try_apply(StorageDelta const& delta);
+    // hash set
 
-	std::optional<StorageObject> const&
-	get() const;
+    uint64_t hs_size_increase = 0;
+    std::vector<Hash> new_hashes;
+    bool do_hs_clear = false;
 
-	std::vector<StorageDelta> get_deltas() const;
+    // std::optional<StorageDelta> overall_delta;
+    bool is_deleted = false;
 
-	// type specific methods
-	// returns nullopt if type mismatch
-	std::optional<int64_t>
-	get_base_nnint64_set_value() const;
+    bool delta_apply_type_guard(StorageDelta const& d) const;
+    void make_current(ObjectType obj_type);
+    void make_current_nnint64(set_add_t const& delta);
+
+  public:
+    ProxyApplicator(std::optional<StorageObject> const& base)
+        : current(base)
+        , memory_write(std::nullopt)
+        , nnint64_delta(std::nullopt)
+        , hs_size_increase(0)
+        , new_hashes()
+        , do_hs_clear(false)
+        , is_deleted(false)
+    {}
+
+    // no-op if result is false, apply if result is true
+    bool __attribute__((warn_unused_result))
+    try_apply(StorageDelta const& delta);
+
+    std::optional<StorageObject> const& get() const;
+
+    std::vector<StorageDelta> get_deltas() const;
+
+    // type specific methods
+    // returns nullopt if type mismatch
+    std::optional<int64_t> get_base_nnint64_set_value() const;
 };
 
-} /* scs */
+} // namespace scs
