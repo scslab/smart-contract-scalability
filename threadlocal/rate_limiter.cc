0e44c45aa1ad1ce879fdfe7143e6f2dcc2b96908
Geoff Ramseyer <gdwr@cs.stanford.edu>
2022-11-27 20:23:19 -0800

    formatting

--- threadlocal/rate_limiter.cc
+++ threadlocal/rate_limiter.cc
@@ -2,6 +2,56 @@
 
 namespace scs {
 
+bool
+RateLimiter::wait_for_opening()
+{
+    auto done = [&]() {
+        return _shutdown.load(std::memory_order_relaxed)
+               || (active_threads < max_active_threads);
+    };
+
+    std::unique_lock lock(mtx);
+
+    auto claim_or_shutdown = [&]() -> bool {
+        if (_shutdown.load(std::memory_order_relaxed)) {
+            return true;
+        }
+        claim_one_slot();
+        notify();
+        return false;
+    };
+
+    if (!done()) {
+        wake_cond.wait(lock, done);
+    }
+    return claim_or_shutdown();
+}
+
+bool
+RateLimiter::fastpath_wait_for_opening()
+{
+    if (active_threads.load(std::memory_order_relaxed)
+        <= max_active_threads.load(std::memory_order_relaxed)) {
+        return false;
+    }
+
+    free_one_slot();
+    return wait_for_opening();
+}
+
+void
+RateLimiter::notify()
+{
+    if (active_threads.load(std::memory_order_relaxed)
+        < max_active_threads.load(std::memory_order_relaxed)) {
+        wake_cond.notify_one();
+    } else // always if max_active_threads == 0, not usually in normal case
+           // operation
+    {
+        notify_join();
+    }
+}
+
 void
 RateLimiter::join_threads()
 {
--- threadlocal/rate_limiter.h
+++ threadlocal/rate_limiter.h
@@ -35,41 +35,9 @@ class RateLimiter
     }
 
   public:
-    bool wait_for_opening()
-    {
-        auto done = [&]() {
-            return _shutdown.load(std::memory_order_relaxed)
-                   || (active_threads < max_active_threads);
-        };
+    bool wait_for_opening();
 
-        std::unique_lock lock(mtx);
-
-        auto claim_or_shutdown = [&]() -> bool {
-            if (_shutdown.load(std::memory_order_relaxed)) {
-                return true;
-            }
-            claim_one_slot();
-            notify();
-            return false;
-        };
-
-        if (!done()) {
-            wake_cond.wait(lock, done);
-        }
-        return claim_or_shutdown();
-    }
-
-    void notify()
-    {
-        if (active_threads.load(std::memory_order_relaxed)
-            < max_active_threads.load(std::memory_order_relaxed)) {
-            wake_cond.notify_one();
-        } else // always if max_active_threads == 0, not usually in normal case
-               // operation
-        {
-            notify_join();
-        }
-    }
+    void notify();
 
     void free_one_slot()
     {
@@ -77,16 +45,7 @@ class RateLimiter
         notify();
     }
 
-    bool fastpath_wait_for_opening()
-    {
-        if (active_threads.load(std::memory_order_relaxed)
-            <= max_active_threads.load(std::memory_order_relaxed)) {
-            return false;
-        }
-
-        free_one_slot();
-        return wait_for_opening();
-    }
+    bool fastpath_wait_for_opening();
 
     void start_threads(uint64_t max_active)
     {
