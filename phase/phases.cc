b8f15d68b5a27981d8e5127c19c81773845f9b5f
Ubuntu <ubuntu@ip-172-31-22-219.us-east-2.compute.internal>
2022-12-09 04:34:28 +0000

    phase instrumentation

--- phase/phases.cc
+++ phase/phases.cc
@@ -8,16 +8,24 @@
 
 #include "transaction_context/global_context.h"
 
+#include <utils/time.h>
+
 namespace scs
 {
 
 void phase_finish_block(GlobalContext& global_structures, BlockContext& block_structures)
 {
+	auto ts = utils::init_time_measurement();
 	block_structures.tx_set.finalize();
+	std::printf("txset finalize %lf\n", utils::measure_time(ts));
 	block_structures.modified_keys_list.merge_logs();
+	std::printf("keylist merge %lf\n", utils::measure_time(ts));
 	global_structures.contract_db.commit();
+	std::printf("contract db commit %lf\n", utils::measure_time(ts));
 	global_structures.state_db.commit_modifications(block_structures.modified_keys_list);
+	std::printf("commit statedb %lf\n", utils::measure_time(ts));
 	ThreadlocalContextStore::post_block_clear();
+	std::printf("post block tlcs clear %lf\n", utils::measure_time(ts));
 }
 
 void phase_undo_block(GlobalContext& global_structures, BlockContext& block_structures)
