0363bc9e7216fb31f5d953d21f967874094534ed
Geoff Ramseyer <gdwr@cs.stanford.edu>
2023-10-30 18:37:10 -0700

    fix linux install

--- wasi/linux_install.sh
+++ wasi/linux_install.sh
@@ -1,4 +1,4 @@
 set -ex
 curl -LO "https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-20/wasi-sdk-20.0-linux.tar.gz"
 tar -xf wasi-sdk-20.0-linux.tar.gz
-#mv wasi-sdk-20.0 wasi-sdk
+mv wasi-sdk-20.0 wasi-sdk
