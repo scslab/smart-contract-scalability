d80d60efeeefb28e443093e1a47407d812803c09
Geoff Ramseyer <geoff.ramseyer@cs.stanford.edu>
2023-12-12 19:26:40 -0800

    fix rdb

--- persistence/rocksdb_wrapper.cc
+++ persistence/rocksdb_wrapper.cc
@@ -4,46 +4,6 @@
 
 namespace scs {
 
-namespace detail {
-
-/**
- * This section adapted from rocksdb/util/stderr_logger.{cc,h}.
- * Original license reproduced below.
- */
-
-//  Copyright (c) Meta Platforms, Inc. and affiliates.
-//
-//  This source code is licensed under both the GPLv2 (found in the
-//  COPYING file in the root directory) and Apache 2.0 License
-//  (found in the LICENSE.Apache file in the root directory).
-
-class OptionsLogger : public rocksdb::Logger
-{
-  public:
-    explicit OptionsLogger()
-        : Logger(rocksdb::InfoLogLevel::INFO_LEVEL)
-    {
-    }
-
-    ~OptionsLogger() override {}
-
-    // Brings overloaded Logv()s into scope so they're not hidden when we
-    // override a subset of them.
-    using rocksdb::Logger::Logv;
-
-    virtual void Logv(const char* format, va_list ap) override
-    {
-        vfprintf(stdout, format, ap);
-        fprintf(stdout, "\n");
-    }
-};
-
-/**
- * End adaptation of rocksdb/util/stderr_logger.{cc,h}
- */
-
-} // namespace detail
-
 rocksdb::Options
 make_options()
 {
@@ -52,8 +12,6 @@ make_options()
     options.create_if_missing = true;
     options.error_if_exists = true;
 
-    detail::OptionsLogger logger;
-    options.Dump(&logger);
     return options;
 }
 
