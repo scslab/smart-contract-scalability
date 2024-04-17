0ad823c0dc4e8f3c9b16272fc0359ed66e030c0a
gramseyer <geoff.ramseyer@cs.stanford.edu>
2024-04-17 03:50:28 +0000

    fix sbrk edge case

--- lficpp/lficpp.cc
+++ lficpp/lficpp.cc
@@ -139,8 +139,12 @@ LFIProc::~LFIProc()
 uintptr_t
 LFIProc::sbrk(uint32_t incr)
 {
+	
 	uintptr_t brkp = brkbase + brksize;
-    if (brkp + incr < base + (4ULL * 1024 * 1024 * 1024)) {
+    	if (incr == 0) {
+		return brkp;
+	}
+	if (brkp + incr < base + (4ULL * 1024 * 1024 * 1024)) {
         void* map;
         if (brksize == 0) {
             map = mmap((void*) brkbase, brksize + incr, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
