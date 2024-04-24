3973f9f8e719bb3b3b312be786690d727cbe3778
Geoff Ramseyer <geoff.ramseyer@cs.stanford.edu>
2024-04-23 19:29:27 -0700

    hook in contract db

---
+++ contract_db/runnable_script.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+
+namespace scs {
+
+struct RunnableScriptView
+{
+    const uint8_t* data;
+    uint32_t len;
+
+    operator bool() { return data == nullptr; }
+};
+
+constexpr static RunnableScriptView null_script{ .data = nullptr, .len = 0 };
+
+} // namespace scs
